3508a6bf28036c7117a86c773612baf2b6ca5432
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-29 17:46:51 +0100

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// var path = require('path');
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -6,7 +5,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-// module.exports = {
   entry: './src/client/index.js',
 
   output: {
@@ -17,14 +15,10 @@ export default {
   mode: 'development',
 
   module: {
-    // loaders: [{
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
-      // query:{
-      //   presets: ["es2015", "react", "stage-0"]
-      // }
       options: {
         presets: ["@babel/preset-env", "@babel/preset-react"]
       }
