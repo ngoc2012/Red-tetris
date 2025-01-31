a429607d565553bb41e5a6ee536375115302edf7
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-31 10:09:44 +0100

    public folder and index.html

--- src/client/containers/app.jsx
+++ src/client/containers/app.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import flyd from 'flyd'
 
 const App = ({message}) => {
-  // Create a stream with initial value 5.
   var number = flyd.stream(5);
-  // Get the current value of the stream.
-  console.log(number()); // logs 5
-  // Update the value of the stream.
+  console.log(number());
   console.log(number(7));
-  // The stream now returns the new value.
-  console.log(number()); // logs 7
+  console.log(number());
   return (
     <span>{message}</span>
   )
