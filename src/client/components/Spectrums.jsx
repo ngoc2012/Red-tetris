7a0adeb8e2df1d1ddbe69f7842a1988a1a3ca44d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2025-02-17 10:55:21 +0100

    spectrums done

--- src/client/components/Spectrums.jsx
+++ src/client/components/Spectrums.jsx
@@ -7,10 +7,9 @@ export const Spectrum = ({info}) => {
       <div className="cells">
       {Array.from({ length: 20 * 10 }).map((_, i) => {
           const row = Math.floor(i / 10);
-          const isBlocked = i < info.penalty;
-          const isFilled = !isBlocked && info.spec[row] && i % 10 < info.spec[row];
-
-          console.log(isBlocked, isFilled, info.spec[row], i % 10, info.spec[row]);
+          const col = i % 10;
+          const isBlocked = row >= (20 - info.penalty);
+          const isFilled = !isBlocked && row >= (20 - info.spec[col]);
 
           return (
             <div
@@ -27,10 +26,11 @@ export const Spectrum = ({info}) => {
 
 export const Spectrums = () => {
   const spectrums = [
-    {playerId: "player 1", score: 12, spec: Array.from({ length: 10 }, () => Math.floor(Math.random() * 20) + 1), penalty: Math.floor(Math.random() * 15) + 1},
+    {playerId: "player 1", score: 12,    spec: Array.from({ length: 10 }, () => Math.floor(Math.random() * 20) + 1), penalty: Math.floor(Math.random() * 15) + 1},
     {playerId: "player 2", score: 24524, spec: Array.from({ length: 10 }, () => Math.floor(Math.random() * 20) + 1), penalty: Math.floor(Math.random() * 15) + 1},
     // {playerId: "player 3", score: 45365, spec: Array.from({ length: 10 }, () => Math.floor(Math.random() * 20) + 1), penalty: Math.floor(Math.random() * 15) + 1}
   ];
+  // console.log(spectrums);
   
   return (
     <div className="spectrum_list">
